3d666ef2fbf7b34220ed55eb02c6c19f789029e6
vsparrow <khan@vsparrow.com>
2018-04-12 18:18:28 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
+  # get '/posts/:id/body', to: 'posts#body'
+  get '/posts/:id/body', to: 'posts#body'
+  # patch 'admin/:id', controller: 'admins', action: :update
+  patch 'posts/:id', controller: 'posts', action: :update
 end
